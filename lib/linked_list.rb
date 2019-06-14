aee0759198c1359a53cf089990d3119754e5fac1
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-14 13:49:56 -0600

    Add Node#set_next to LinkedList#append, #count and #last_node method to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -10,6 +10,33 @@ class LinkedList
   def append(data)
     if @head == nil
       @head = Node.new(data)
+    else
+      last_node.set_next(Node.new(data))
     end
+  end
+
+  def count
+    nodes = 0
+    current_node = @head
+    if @head != nil
+      nodes = 1
+      until current_node.next_node == nil
+        nodes += 1
+        current_node = current_node.next_node
+      end
+    end
+    nodes
+  end
+
+  private
+
+  def last_node
+    if @head != nil
+      current_node = @head
+      until current_node.next_node == nil
+        current_node = current_node.next_node
+      end
+    end
+    current_node
   end
 end
