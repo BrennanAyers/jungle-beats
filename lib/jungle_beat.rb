3fd363895aa875527e4b0ee6a51f056c6a20b782
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-17 13:42:50 -0600

    Add #append method to JungleBeat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -3,8 +3,13 @@ require './lib/linked_list'
 
 class JungleBeat
   attr_reader :list
-  
+
   def initialize
     @list = LinkedList.new
+  end
+
+  def append(sounds)
+    split_sounds = sounds.split(" ")
+    split_sounds.each {|sound| @list.append(sound)}
   end
 end
