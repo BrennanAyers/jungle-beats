7a9f1d01bcded7612009c441699d7868adcbdb3f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-17 13:07:27 -0600

    Add #append spec to JungleBeat

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -10,4 +10,12 @@ describe 'Jungle Beat' do
     expect(@jungle_beat.list).to_not eq(nil)
     expect(@jungle_beat.list.head).to eq(nil)
   end
+
+  it 'can append multiple sounds' do
+    @jungle_beat.append("boom bam bip")
+
+    expect(@jungle_beat.list.head.data).to eq("boom")
+    expect(@jungle_beat.list.next_node.data).to eq("bam")
+    expect(@jungle_beat.count).to eq(3)
+  end
 end
